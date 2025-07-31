0754b752800e03dbb7700a9559012f91d355501a
sanketshinde <sanket.shinde@indexnine.com>
2025-07-31 19:17:16 +0530

    Remove unused user and tasks repositories

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TasksRepository } from './tasks.repository';
 import { Task } from './tasks.entity';
 import { AuthModule } from 'src/auth/auth.module';
 
